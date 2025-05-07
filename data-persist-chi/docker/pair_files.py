92829ed44fe3415c9cdb63566057f9467c152c93
Neha Patil <76066600+patilneha08@users.noreply.github.com>
2025-05-06 20:41:05 -0400

    Update pair_files.py

--- data-persist-chi/docker/pair_files.py
+++ data-persist-chi/docker/pair_files.py
@@ -3,7 +3,7 @@ import shutil
 
 wav_dir = "nptelfinal/wav"
 txt_dir = "nptelfinal/txt"
-out_dir = "nptelfinal/paired"
+out_dir = "/data/paired"
 
 # Ensure output directory exists
 os.makedirs(out_dir, exist_ok=True)
